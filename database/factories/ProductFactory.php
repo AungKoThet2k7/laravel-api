8bce724554de4cac38317c5a8f2dc99f80068ab7
AungKoThet2k7 <aungkothat.mgn19@gmail.com>
2025-10-20 15:32:00 +0630

    add product seeder & user seeder

--- database/factories/ProductFactory.php
+++ database/factories/ProductFactory.php
@@ -2,6 +2,7 @@
 
 namespace Database\Factories;
 
+use App\Models\User;
 use Illuminate\Database\Eloquent\Factories\Factory;
 
 /**
@@ -17,7 +18,10 @@ class ProductFactory extends Factory
     public function definition()
     {
         return [
-            //
+            "name" => $this->faker->name(),
+            "price" => $this->faker->numberBetween(500, 10000),
+            "stock" => $this->faker->numberBetween(5, 30),
+            "user_id" => User::inRandomOrder()->first()->id,
         ];
     }
 }
